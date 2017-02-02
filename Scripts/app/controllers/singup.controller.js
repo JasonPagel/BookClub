24082a84b5d51f1e4ae54d390f95731715505d04
Jason Pagel <jason.e.pagel@gmail.com>
2017-02-02 10:48:26 -0600

    removes console output of user when sign up

--- Scripts/app/controllers/singup.controller.js
+++ Scripts/app/controllers/singup.controller.js
@@ -9,15 +9,12 @@
     function signupController($scope, Analytics, axcesshttp) {
 
         var vm = this;
-        
+
         Analytics.trackPage('/DemoApp/Signup', 'DemoApp');
 
         vm.cancel = $scope.$dismiss;
 
         vm.signUp = function () {
-
-            console.dir(vm.user);
-
             var config = {url: 'api/signup', data: vm.user};
 
             axcesshttp.post('api/signup', vm.user, config).then(function (response) {
